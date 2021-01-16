27b0b4342f2521770469286fd8989c3b32fe7368
Jose Miguel Valiente <valiente_jose2@hotmail.com>
2021-01-16 19:10:04 +0100

    Test api

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const fs = require('fs')
 const multer = require('multer')
 const { createWorker } = require('tesseract.js')
 const PORT = 5000 || process.env.PORT
+
+app.use(cors())
 
 const worker = createWorker({
     logger: m => console.log(m)
@@ -29,19 +32,23 @@ app.get("/", (req, res, next) => {
 
 app.post("/upload", (req, res, next) => {
     upload(req, res, next => {
-        fs.readFile(`./upload/${req.file.originalname}`, (err, data) => {
+        fs.readFile(`./upload/${req.file.originalname}`, async (err, data) => {
             if (err) return console.log(err)
-            console.log(data)
-            const trasnsform = async function () {
+            async function transform() {
                 await worker.load();
                 await worker.loadLanguage('spa');
                 await worker.initialize('spa');
                 const { data: { text } } = await worker.recognize(`./upload/${req.file.originalname}`);
-                console.log(text);
-                await worker.terminate();
-            }()
+                console.log(text)
+                //console.log(text.indexOf("AHORRAMAS"));
+                //console.log(text.split("").slice(75).join(""))
+                //await worker.terminate();
+                return text
+            }
+            await transform()
+                .then(el => { res.send({ data: el }) })
+                .catch(err => { res.send({ msg: err }) })
 
-            trasnsform.then(el => console.log(el))
 
         })
     })
